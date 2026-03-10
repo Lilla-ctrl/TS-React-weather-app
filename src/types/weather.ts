9f8bc4b7e7cb92e46c676a10fa0e47e6c5a84fdc
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-03-10 14:44:11 +0100

    create weather.ts for global types

---
+++ src/types/weather.ts
@@ -0,0 +1,26 @@
+export interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
+export interface WeatherData {
+  temperature: number;
+  wind: number;
+  humidity: number;
+  city: string;
+  description: string;
+  icon: string;
+  coordinates: Coordinates;
+  timezone: string;
+}
+
+export interface ForecastDay {
+  icon: string;
+  description: string;
+  maxTemp: number;
+  minTemp: number;
+}
+
+export type Forecast = ForecastDay[];
+
+export type Unit = "Celsius" | "Fahrenheit";
