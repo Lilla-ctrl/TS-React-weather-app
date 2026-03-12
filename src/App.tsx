8f8be57423d1f93f908caafc08bae6e519bc777a
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-03-12 20:10:39 +0100

    create Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import "./App.css";
 import Weather from "./components/Weather";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="bg-secondary py-5">
       <div className="max-w-150 mx-auto bg-primary rounded-lg">
         <Weather />
-        <footer>
-          Coded by Lilla,{" "}
-          <a
-            href="https://github.com/Lilla-ctrl/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-source code
-          </a>{" "}
-          on GitHub.
-        </footer>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      Coded by Lilla,{" "}
+      <a
+        href="https://github.com/Lilla-ctrl/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open-source code
+      </a>{" "}
+      on GitHub.
+    </footer>
+  );
+}
