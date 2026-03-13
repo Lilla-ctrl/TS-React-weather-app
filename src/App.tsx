2cbbc66d6fd0ab3ded49bf907b49a385431386fa
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-03-13 14:45:57 +0100

    add Toaster to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Weather from "./components/Weather";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
     <div className="bg-secondary min-h-screen py-5 px-2">
       <div className="max-w-150 mx-auto bg-primary rounded-lg">
+        <Toaster />
         <Weather />
         <Footer />
       </div>
