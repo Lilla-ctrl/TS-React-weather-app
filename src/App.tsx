dd1a025cb82b0c1cd3574f667a8787050fa4ea0e
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-03-13 10:44:59 +0100

    update App styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="bg-secondary py-5">
+    <div className="bg-secondary py-5 px-2">
       <div className="max-w-150 mx-auto bg-primary rounded-lg">
         <Weather />
         <Footer />
